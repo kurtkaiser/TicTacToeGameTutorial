d1005d49fff46afb01c9da1e44a688a74b32a98e
Kurt Kaiser <kurtbkaiser@gmail.com>
2020-01-27 16:43:32 -0500

    Add files via upload

---
+++ Scripts/GameScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    int spriteIndex = -1;
+
+    public int PlayerTurn()
+    {
+        spriteIndex++;
+        return spriteIndex % 2;
+    }
+}
---
+++ Scripts/TurnScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnScript : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    GameObject gameBoard;
+    public Sprite[] images;
+    bool unplayed = true;
+
+    private void Start()
+    {
+        spriteRenderer.sprite = null;
+    }
+
+    private void OnMouseDown()
+    {
+        if (unplayed)
+        {
+            int index = gameBoard.GetComponent<GameScript>().PlayerTurn();
+            spriteRenderer.sprite = images[index];
+            unplayed = false;
+        }
+    }
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        gameBoard = GameObject.Find("GameBoard");
+    }
+}
